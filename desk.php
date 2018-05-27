149cf46b9f8f8d8be3529076582d0c464e79fa99
leoxiaoge <1269286430@qq.com>
2018-05-27 18:45:16 +0800

    first commit

---
+++ desk.php
@@ -0,0 +1,6 @@
+<?php 
+// $Shortcut= "[InternetShortcut] URL='https://www.qiaolibeilang.com/manager' IDList=IconIndex=43 IconFile=/favicon.ico HotKey=1626 [{000214A0-0000-0000-C000-000000000046}] Prop3=19,2";
+//  Header("Content-type: application/octet-stream"); 
+//  header("Content-Disposition: attachment; filename=宿洗.url");
+//  echo $Shortcut;
+ $Shortcut= "[InternetShortcut] URL='你的域名' IDList=IconIndex=43 IconFile=/favicon.ico HotKey=1626 [{000214A0-0000-0000-C000-000000000046}] Prop3=19,2"; Header("Content-type: application/octet-stream"); header("Content-Disposition: attachment; filename=银行利率.url"); echo$Shortcut; 
